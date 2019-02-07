68402afe738016fe4e2ab73ac22d43050ff19b68
Sebastian Tello <argos83@gmail.com>
2019-02-07 20:19:51 +1100

    fixed contact form

--- js/agency.js
+++ js/agency.js
@@ -4,6 +4,11 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+
+$(window).load(function () {
+    emailjs.init('user_zrpCVofqoupw5dx1HBxww');
+});
+
 // jQuery to keep track of the current section when switching languages
 $("ul.nav").on('activate.bs.scrollspy', function () {
    var hash = $(this).find("li.active a").attr("href");
@@ -46,4 +51,36 @@ $('div.modal').on('show.bs.modal', function() {
 			$(modal).modal('hide');
 		}
 	}
+});
+
+//Contact form
+function onContactSuccess() {
+    $('#contact-form')[0].reset();
+
+    $('#success').html("<div class='alert alert-success'>");
+    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+    $('#success > .alert-success')
+        .append("<strong>Thanks for your message, we'll be in touch soon!</strong>");
+    $('#success > .alert-success')
+        .append('</div>');
+}
+
+function onContactError(error) {
+    console.log(error);
+    $('#success').html("<div class='alert alert-danger'>");
+    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+    $('#success > .alert-danger').append("<strong>Your message could not be sent, try again later or send us an email to <a href='mailto:hello@centreofsense.com'>hello@centreofsense.com</a>.</strong>");
+    $('#success > .alert-danger').append('</div>');
+}
+
+$('#contact-form').submit(function (e) {
+    e.preventDefault();
+    emailjs.sendForm('default_service', 'contact_form', this)
+        .then(function () {
+            onContactSuccess();
+        }, function (error) {
+            onContactError(error);
+        });
 });
